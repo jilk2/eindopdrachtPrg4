d23dcd0d19d92da4b8b268afdea1ba2ace922e76
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-05-28 08:28:02 +0200

    removed not used images and added soldier image + added in resources.js

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,8 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
-// voeg hier jouw eigen resources toe
+
 const Resources = {
-    Fish: new ImageSource('images/fish.png'),
-    Shark: new ImageSource('images/shark.png')
+    Soldier: new ImageSource("images/soldier.png")
 }
 
 
