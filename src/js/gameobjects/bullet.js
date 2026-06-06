2c2f1137c5dafb7f5427d6df16140d66971782a3
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-06-06 16:11:52 +0200

    formated bullet + zombie files

--- src/js/gameobjects/bullet.js
+++ src/js/gameobjects/bullet.js
@@ -1,23 +1,42 @@
-import { Actor, Engine, Vector, Scene } from "excalibur"
-import { Resources } from '../resources.js'
+import { Actor, Engine, Vector, Scene, CollisionType } from "excalibur";
+import { Resources } from "../resources.js";
+import { Zombie } from "./zombie.js";
 
-export class Bullet extends Actor{
-    speed = 400
-    amount = 10
-    constructor(x,y, playerRotation){
-        super({x,y, width:10, height:10})
-        this.events.on("exitviewport", () => this.kill())
+export class Bullet extends Actor {
+  speed = 400;
+  amount = 10;
+  constructor(x, y, playerRotation) {
+    super({
+      x,
+      y,
+      width: 10,
+      height: 10,
+      collisionType: CollisionType.Passive,
+    });
+    this.events.on("exitviewport", () => this.kill());
+    this.events.on("collisionstart", this.attack.bind(this));
+  }
 
+  onInitialize(engine) {
+    this.graphics.use(Resources.Bullet.toSprite());
+  }
+
+  addBullet(x, y, playerRotation) {
+    this.pos.x = x;
+    this.pos.y = y;
+    this.vel = new Vector(
+      Math.cos(playerRotation) * this.speed,
+      Math.sin(playerRotation) * this.speed,
+    );
+    this.rotation = playerRotation + Math.PI / 2;
+  }
+
+  attack(event) {
+    const other = event.other.owner;
+    if (other instanceof Zombie) {
+      console.log("I hit a zombie");
+      other.damage();
+      this.kill();
     }
-
-    onInitialize(engine){
-        this.graphics.use(Resources.Bullet.toSprite())
-    }
-
-addBullet(x,y, playerRotation) {
-    this.pos.x = x
-    this.pos.y = y
-    this.vel = new Vector(Math.cos(playerRotation)*this.speed, Math.sin(playerRotation)*this.speed)
-    this.rotation = playerRotation + Math.PI/2
-}
+  }
 }
--- src/js/gameobjects/zombie.js
+++ src/js/gameobjects/zombie.js
@@ -1,30 +1,45 @@
-import { Actor, Engine, Vector, randomInRange, CollisionType} from "excalibur"
-import { Resources } from '../resources.js'
-import { Soldier } from "./soldier.js"
-
+import {
+  Actor,
+  Engine,
+  Vector,
+  randomInRange,
+  CollisionType,
+  randomIntInRange,
+} from "excalibur";
+import { Resources } from "../resources.js";
+import { Soldier } from "./soldier.js";
 
 export class Zombie extends Actor {
+  hitpoints;
 
-    constructor() {
-        super({ height:50, width: 50,collisionType: CollisionType.Active })
-        console.log("I am spawned")
-    }
-
-    /**
-     * setup for the soldier that the player controlls
-     **/
-
-    onInitialize(engine) {
-        const zombie = new Actor()
-        this.graphics.use(Resources.Zombie.toSprite())
-        this.on('collisionstart', (event) => this.attack(event))
-        this.pos = new Vector(Math.random() * 1280, Math.random() * 720)
-    }
-
-attack(event) {
-  const other = event.other.owner
-  if (other instanceof Soldier) {
-    other.damage()
+  constructor() {
+    super({ height: 50, width: 50, collisionType: CollisionType.Active });
+    this.hitpoints = randomIntInRange(1, 5);
+    console.log("I am spawned");
   }
-}
+
+  /**
+   * setup for the soldier that the player controlls
+   **/
+
+  onInitialize(engine) {
+    const zombie = new Actor();
+    this.graphics.use(Resources.Zombie.toSprite());
+    this.on("collisionstart", (event) => this.attack(event));
+    this.pos = new Vector(Math.random() * 1280, Math.random() * 720);
+  }
+
+  damage() {
+    this.hitpoints -= 4;
+    if (this.hitpoints <= 0) {
+      this.kill();
+    }
+  }
+
+  attack(event) {
+    const other = event.other.owner;
+    if (other instanceof Soldier) {
+      other.damage();
+    }
+  }
 }
