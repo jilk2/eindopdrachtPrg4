ca5f349a28c0b15020e53f2d1d29c1de4328dc8c
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-06-12 19:27:21 +0200

    remove unnecassery code and formatted level.js

--- src/js/scenes/level.js
+++ src/js/scenes/level.js
@@ -1,52 +1,45 @@
-import { Actor, Engine, Vector, Scene, Timer, randomInRange } from "excalibur"
-import { Resources } from '../resources.js'
-import { Background } from "../background.js"
-import { Soldier } from '../gameobjects/soldier.js'
-import { Bullet } from '../gameobjects/bullet.js'
-import { Zombie } from "../gameobjects/zombie.js"
+import { Actor, Engine, Vector, Scene, Timer, randomInRange } from "excalibur";
+import { Resources } from "../resources.js";
+import { Background } from "../background.js";
+import { Soldier } from "../gameobjects/soldier.js";
+import { Bullet } from "../gameobjects/bullet.js";
+import { Zombie } from "../gameobjects/zombie.js";
 
-export class Level extends Scene{
+export class Level extends Scene {
+  onInitialize(engine) {
+    console.log("this level is created only once.");
+    const background = new Background(Resources.Background);
 
-    
-    onInitialize(engine) {
-        console.log("this level is created only once.")
-        const background = new Background(Resources.Background)
-        this.add(background);
-        console.log('Starting Game');
+    this.add(background);
+    console.log("Starting Game");
+
+    this.playerOne = new Soldier(200, 200, "1");
+    this.add(this.playerOne);
+
+    this.spawnZombies()
 
 
-        this.playerOne = new Soldier(200, 200, "1")
-        this.add(this.playerOne);
-
-        this.spawnZombies()
-
-        // this.playerOne.events.on("exitviewport", (e) => this.keepInScreen(e));
-        // this.ui = new UI()
-        // this.add(this.ui)
     this.zombieTimer = new Timer({
       fcn: () => {
-        this.spawnZombies()
+        this.spawnZombies();
       },
       repeats: true,
-      interval: 5000
-    })
-    this.add(this.zombieTimer)
-    this.zombieTimer.start()
+      interval: 5000,
+    });
+    this.add(this.zombieTimer);
+    this.zombieTimer.start();
+  }
+
+  spawnZombies() {
+    for (let index = 0; index < randomInRange(1, 5); index++) {
+      this.zombie = new Zombie();
+      this.add(this.zombie);
     }
-    
-spawnZombies(){
-    for (let index = 0; index < randomInRange(1,5); index++) {
-        this.zombie = new Zombie()
-        this.add(this.zombie)
-    }
-}
+  }
 
-    
-
-    spawnBullet(x,y,rot){
-    const b = new Bullet()
-    b.addBullet(x,y,rot)
-    this.add(b)
-}
-
+  spawnBullet(x, y, rot) {
+    const bullet = new Bullet();
+    bullet.addBullet(x, y, rot);
+    this.add(bullet);
+  }
 }
